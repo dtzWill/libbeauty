cf256274eed2d2b4205976cd54ea938431ace5b2
jcdutton <James.Dutton@gmail.com>
2017-04-15 11:33:17 +0100

    Fix instruction decoder.

--- libbeauty/src/input/instruction_decoder/llvm/amd64/X86BaseInfo.h
+++ libbeauty/src/input/instruction_decoder/llvm/amd64/X86BaseInfo.h
@@ -576,7 +576,7 @@ namespace X86II {
   /// of the specified instruction.
   inline unsigned getSizeOfImm(uint64_t TSFlags) {
     switch (TSFlags & X86II::ImmMask) {
-    default: llvm_unreachable("Unknown immediate size");
+    default: return 0;
     case X86II::Imm8:
     case X86II::Imm8PCRel:
     case X86II::Imm8Reg:    return 1;
